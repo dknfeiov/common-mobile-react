ed9ec64833885b4ed95e54295bc1de747440a17a
dknfeiov <610604963@qq.com>
2018-05-19 18:25:15 +0800

    “包依赖添加”

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Record from './views/record';
+import {Tabs, Badge} from 'antd-mobile';
 
 // ReactDOM.render(<Button>Start</Button>, mountNode);
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.tabs = [
+      { title: <Badge text={'3'}>日常任务</Badge> },
+      { title: <Badge text={'今日(20)'}>社区</Badge> },
+      { title: <Badge dot>我的</Badge> },
+    ];
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,6 +27,12 @@ class App extends Component {
         <div className="body">
           <Record></Record>
         </div>
+        <Tabs tabs={this.tabs} tabBarPosition='bottom'
+          initialPage={1}
+          onChange={(tab, index) => { console.log('onChange', index, tab); }}
+          onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+        >
+        </Tabs>
       </div>
     );
   }
